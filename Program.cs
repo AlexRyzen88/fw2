01298f8910809142e20454d48ab39d872ee77d3e
Alex <xath@mail.ru>
2023-01-13 18:28:43 +0300

    fix code and clear

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
 string[] FindLessThan(string[] input, int n) {
     string[] output = new string[CountLessThan(input, n)];
 
-    for(int i = 0, j = 0; i < input.Length; i++) {
+    for(int i = 0, j = 0; i < input.Length; i++)
+     {
         if(input[i].Length <= n) {
             output[j] = input[i];
             j++;
@@ -21,7 +22,8 @@ string[] FindLessThan(string[] input, int n) {
 int CountLessThan(string[] input, int n) {
     int count = 0;
 
-    for(int i = 0; i < input.Length; i++) {
+    for(int i = 0; i < input.Length; i++) 
+    {
         if(input[i].Length <= n) {
             count++;
         }
